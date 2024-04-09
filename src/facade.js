f816d4398651825f7e2a35db0dabc4df553c39d8
Igor Batista Lima <112262600+IgorBatistaLima@users.noreply.github.com>
2024-04-08 22:33:55 -0300

    Update facade.js

--- src/facade.js
+++ src/facade.js
@@ -1,1 +1,48 @@
+const Contato  = require('./contato');
+const GerenciadorContatos  = require('./gerenciadorContatos');
 
+class Facade {
+  constructor() {
+      this.gerenciador = new GerenciadorContatos();
+  }
+
+  adicionarContato(nome, telefone, email) {
+      this.gerenciador.adicionarContato(new Contato(nome, telefone, email));
+  }
+
+  removerContato(nome) {
+      this.gerenciador.removerContato(nome);
+  }
+
+  listarContatos() {
+      return this.gerenciador.listarContatos();
+  }
+
+  buscarContato(nome) {
+      return this.gerenciador.buscarContato(nome);
+  }
+
+  definirBuscaStrategy(strategy) {
+      this.gerenciador.definirBuscaStrategy(strategy);
+  }
+}
+
+class BuscaStrategy {
+  buscar(contatos, nome) {
+      throw new Error('Método "buscar" não implementado');
+  }
+}
+
+class BuscaSimplesStrategy extends BuscaStrategy {
+  buscar(contatos, nome) {
+      return contatos.find(contato => contato.nome === nome);
+  }
+}
+
+class BuscaAvancadaStrategy extends BuscaStrategy {
+  buscar(contatos, nome) {
+        return contatos.filter(contato => contato.nome.includes(nome));
+  }
+}
+
+module.exports = { Facade, BuscaSimplesStrategy, BuscaAvancadaStrategy };
